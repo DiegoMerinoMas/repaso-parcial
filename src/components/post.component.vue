4152919474c46ed9c65c3550fd1026769f9eeb31
DiegoMerinoMas <u202111473@upc.edu.pe>
2023-05-11 22:59:53 -0500

    simple card para el orden

--- src/components/post.component.vue
+++ src/components/post.component.vue
@@ -1,18 +1,46 @@
 <template>
-    <p>Hola</p>
+    <pv-card>
+        <template #header>
+            <h2>{{ post.title }}</h2>
+        </template>
+        <template #title>
+            <p>{{ post.resume }}</p>
+            <p>{{ post.content }}</p>
+        </template>
+        <template #footer>
+            <img :src="post.photoUrl" height="300" width="300" />
+            <p> likes 👍 : {{ post.likes }}</p>
+        </template>
+    </pv-card>
 </template>
 
 <script>
 import { PostApiService } from '../services/post-api.services';
 
-export default{
-    data(){
-        return{
-            post:{},
+export default {
+    name: 'Post',
+    data() {
+        return {
+            post: {},
         };
+    },
+    created() {
+        this.fetchPostData();
+    },
+    methods: {
+        fetchPostData() {
+            const postId = this.$route.params.id; // Obtener el ID del post desde la ruta
+            const postService = new PostApiService();
+            postService.getPostById(postId)
+                .then(response => {
+                    this.post = response.data; // Asignar los datos del post al objeto post
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
 }
 </script>
 
-<style>
-</style>
+<style></style>
